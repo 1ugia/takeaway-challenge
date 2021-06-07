5af2d0d60a142b41f25afc53ca12b3e1129e33f3
Masako Gray <masakogray@hotmail.co.uk>
2021-06-07 03:20:49 +0100

    1st user story: menu comes up

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,11 @@
 class Takeaway
 
+  def menu
+    {
+      'sashimi': 5.60,
+      'nigiri': 6.50,
+      'inari': 3.50
+    }
+  end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,12 @@
 require 'takeaway'
 
 describe Takeaway do
+  let(:menu){subject.menu}
+  it 'lists menu' do
+    expect(subject.menu).to include(menu)
+  end
 
+  it 'lets you pick menues' do
+    expect.(subject.pick).to eq
+  end
 end
